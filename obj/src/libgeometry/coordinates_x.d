01a5fe8a05208d901f73347816ac04bf575352e2
Латрыгин Арсений <latrygin160902@gmail.com>
2021-04-09 01:43:54 +0700

    ///

---
+++ obj/src/libgeometry/coordinates_x.d
@@ -0,0 +1,2 @@
+obj/src/libgeometry/coordinates_x.o: src/libgeometry/coordinates_x.c \
+ src/libgeometry/function.h
---
+++ obj/src/libgeometry/coordinates_y.d
@@ -0,0 +1,2 @@
+obj/src/libgeometry/coordinates_y.o: src/libgeometry/coordinates_y.c \
+ src/libgeometry/function.h
